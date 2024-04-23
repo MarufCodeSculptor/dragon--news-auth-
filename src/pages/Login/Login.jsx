439153a27bbbd8ce300bace3ea8d227ecc5b58f0
Maruf-Raju-pro <Muhammedmarufraju@gmail.com>
2024-04-23 15:28:05 +0600

    first commit

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,15 @@
+import Navbar from '../Shared/Navbar/Navbar';
+
+const Login = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+
+      <div>
+        <h3 className="text-3xl text-center font-bold">Login loading....</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
